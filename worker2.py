41082895f4af2d36f6707ca6f7616391b342aa37
Renming <rmqi@live.com>
2017-03-06 02:59:07 -0800

    First Commit. Worker 1 runs first.

---
+++ worker2.py
@@ -0,0 +1,158 @@
+'''
+Save and Restore a model using TensorFlow.
+This example is using the MNIST database of handwritten digits
+(http://yann.lecun.com/exdb/mnist/)
+
+Author: Aymeric Damien
+Project: https://github.com/aymericdamien/TensorFlow-Examples/
+'''
+
+from __future__ import print_function
+
+
+
+# Import MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+import tensorflow as tf
+import time
+import os
+import numpy as np
+
+#
+model_path = "/Users/Renming/Dropbox/Apps/model.ckpt"
+model_path2 = "/Users/Renming/Dropbox/Apps/model2.ckpt"
+sub1 = '.index'
+name1 = model_path + sub1
+sub2 = '.meta'
+name2 = model_path + sub2
+sub3 = '.data-00000-of-00001'
+name3 = model_path + sub3
+names = [name1,name2,name3]
+mtime_last = [0,0,0]
+
+
+
+def getNewTime():
+    mtime_cur = []
+    for name in names:
+        mtime_cur.append(os.path.getmtime(name))
+    return mtime_cur
+
+
+def isFilesCreated():
+    res = True
+    for name in names:
+        if not os.path.isfile(name):
+            res = False
+    return res
+
+# Parameters
+learning_rate = 0.001
+batch_size = 100
+display_step = 1
+
+# Network Parameters
+n_hidden_1 = 256 # 1st layer number of features
+n_hidden_2 = 256 # 2nd layer number of features
+n_input = 784 # MNIST data input (img shape: 28*28)
+n_classes = 10 # MNIST total classes (0-9 digits)
+
+# tf Graph input
+x = tf.placeholder("float", [None, n_input])
+y = tf.placeholder("float", [None, n_classes])
+
+
+# Create model
+def multilayer_perceptron(x, weights, biases):
+    # Hidden layer with RELU activation
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)
+    # Hidden layer with RELU activation
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+    # Output layer with linear activation
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    return out_layer
+
+# Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+# Construct model
+pred = multilayer_perceptron(x, weights, biases)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+# 'Saver' op to save and restore all the variables
+saver = tf.train.Saver()
+
+
+def load_run_save():
+    with tf.Session() as sess:
+
+        sess.run(init)
+        if not isFilesCreated():
+            print('model have not been trained' )
+        else:
+            saver.restore(sess, model_path )
+            print('load trained model')
+
+    # Training cycle
+        for epoch in range(7):
+            avg_cost = 0.
+            total_batch = int(mnist.train.num_examples / batch_size)
+            # Loop over all batches
+            for i in range(total_batch):
+                batch_x, batch_y = mnist.train.next_batch(batch_size)
+                # Run optimization op (backprop) and cost op (to get loss value)
+                _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
+                                                              y: batch_y})
+                # Compute average loss
+                avg_cost += c / total_batch
+            # Display logs per epoch step
+            if epoch % display_step == 0:
+                print("Epoch:", '%04d' % (epoch + 1), "cost=", \
+                      "{:.9f}".format(avg_cost))
+        print("Second Optimization Finished!")
+
+        # Test model
+        correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+        # Calculate accuracy
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        print("Accuracy:", accuracy.eval(
+            {x: mnist.test.images, y: mnist.test.labels}))
+        save_path = saver.save(sess, model_path)
+        print("Model saved in file: %s" % save_path)
+
+
+
+def watchFileChanges(mtime_last):
+    while True:
+        time.sleep(5)
+        if isFilesCreated():
+            mtime_cur = getNewTime()
+            if not np.array_equal(mtime_last,mtime_cur):
+                print('file changed')
+                load_run_save()
+                mtime_last = getNewTime()
+
+
+watchFileChanges(mtime_last)
+
+
+
